825994bb74d0671488a30db32b459a3eee1577f7
MARSELVARDANYAN <marseli-93@mail.ru>
2025-06-29 00:42:48 +0400

    update

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,7 @@
 //   login,
 //   validateToken,
 //   getCurrentUser,
-  
+
 // } from "../controllers/auth.js";
 // const router = Router();
 
@@ -16,7 +16,6 @@
 
 // export default router;
 
-
 import { Router } from "express";
 import passport from "passport";
 import { auth } from "../middleware/auth.js";
@@ -25,7 +24,7 @@ import {
   login,
   validateToken,
   getCurrentUser,
-  logout
+  logout,
 } from "../controllers/auth.js";
 import jwt from "jsonwebtoken";
 
@@ -36,33 +35,49 @@ router.post("/login", login);
 router.get("/logout", auth, logout);
 
 router.get("/google", (req, res, next) => {
-  const state = req.query.redirect || '/';
+  console.log(
+    "Initiating Google OAuth with redirect_uri:",
+    "https://photo-editor-1.onrender.com/api/auth/google/callback"
+  );
+  const state = req.query.redirect || "/";
   passport.authenticate("google", {
     scope: ["profile", "email"],
     prompt: "select_account",
     accessType: "offline",
-    state: state 
+    state: state,
   })(req, res, next);
 });
 
-
-router.get("/google/callback", 
+router.get(
+  "/google/callback",
   passport.authenticate("google", { session: false }),
   (req, res) => {
+    console.log("Received Google callback with code:", req.query.code);
+    console.log("State:", req.query.state);
     const token = generateToken(req.user);
-    const state = req.query.state ? decodeURIComponent(req.query.state) : '/';
-    res.redirect(`${process.env.CLIENT_URL}/oauth-redirect?token=${encodeURIComponent(token)}&state=${encodeURIComponent(state)}`);
+    const state = req.query.state ? decodeURIComponent(req.query.state) : "/";
+    res.redirect(
+      `${process.env.CLIENT_URL}/oauth-redirect?token=${encodeURIComponent(
+        token
+      )}&state=${encodeURIComponent(state)}`
+    );
   }
 );
 
-
-
-router.get("/facebook", passport.authenticate("facebook", { scope: ["email"] }));
-router.get("/facebook/callback", 
+router.get(
+  "/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+router.get(
+  "/facebook/callback",
   passport.authenticate("facebook", { session: false }),
   (req, res) => {
     const token = generateToken(req.user);
-  res.redirect(`${process.env.CLIENT_URL}/oauth-redirect?token=${encodeURIComponent(token)}`);
+    res.redirect(
+      `${process.env.CLIENT_URL}/oauth-redirect?token=${encodeURIComponent(
+        token
+      )}`
+    );
   }
 );
 
@@ -70,11 +85,9 @@ router.get("/validate", auth, validateToken);
 router.get("/me", auth, getCurrentUser);
 
 const generateToken = (user) => {
-  return jwt.sign(
-    { user: { id: user.id } },
-    process.env.JWT_SECRET,
-    { expiresIn: "24h" }
-  );
+  return jwt.sign({ user: { id: user.id } }, process.env.JWT_SECRET, {
+    expiresIn: "24h",
+  });
 };
 
 export default router;
