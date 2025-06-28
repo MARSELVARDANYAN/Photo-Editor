37dbfcc2e1ff46b8b9201b7cca1a4bc7c5921bd5
MARSELVARDANYAN <marseli-93@mail.ru>
2025-06-29 02:28:56 +0400

    update

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
     "script-src 'self' 'unsafe-inline'; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " + // Разрешаем inline-стили
     "font-src 'self' https://fonts.gstatic.com; " +
-    "img-src 'self' data: blob:; " +
+    "img-src 'self' data: blob: https://images.unsplash.com; " +
     "connect-src *"
   );
   next();
